4ae5cc53801bf1110dea9e026d5066d198f37bec
Liwa Alex Saputra <liwaalex99@gmail.com>
2018-10-16 13:14:36 +0700

    Initial commit

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "60%";
+    $("#mySidenav").show('fade');
+    //document.body.style.backgroundColor = "rgba(0,0,0,0.3)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    //document.body.style.backgroundColor = "#FFFAE6";
+}
